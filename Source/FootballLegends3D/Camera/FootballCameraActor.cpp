a0f9ca41548a156c3f00fb929921a76ca21770cc
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 09:19:58 +0100

    feat: implement smooth runtime football camera

--- Source/FootballLegends3D/Camera/FootballCameraActor.cpp
+++ Source/FootballLegends3D/Camera/FootballCameraActor.cpp
@@ -1,13 +1,42 @@
 #include "Camera/FootballCameraActor.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/Actor.h"
 
 AFootballCameraActor::AFootballCameraActor()
 {
-    PrimaryActorTick.bCanEverTick = false;
+    PrimaryActorTick.bCanEverTick = true;
 
     if (UCameraComponent* Camera = GetCameraComponent())
     {
-        Camera->FieldOfView = 45.0f;
+        Camera->FieldOfView = FieldOfView;
         Camera->bConstrainAspectRatio = false;
     }
+}
+
+void AFootballCameraActor::SetFollowTarget(AActor* NewTarget)
+{
+    FollowTarget = NewTarget;
+}
+
+void AFootballCameraActor::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    if (!IsValid(FollowTarget))
+    {
+        return;
+    }
+
+    const FVector TargetLocation = FollowTarget->GetActorLocation();
+    const FVector TargetForward = FollowTarget->GetActorForwardVector().GetSafeNormal2D();
+    const FVector CameraLocation = TargetLocation - (TargetForward * FollowDistance) + FVector::UpVector * FollowHeight;
+    const FVector LookAtLocation = TargetLocation + (TargetForward * LookAheadDistance);
+
+    const float PositionAlpha = 1.0f - FMath::Exp(-PositionInterpSpeed * DeltaSeconds);
+    const float RotationAlpha = 1.0f - FMath::Exp(-RotationInterpSpeed * DeltaSeconds);
+
+    SetActorLocation(FMath::Lerp(GetActorLocation(), CameraLocation, PositionAlpha));
+
+    const FRotator DesiredRotation = (LookAtLocation - GetActorLocation()).Rotation();
+    SetActorRotation(FMath::RInterpTo(GetActorRotation(), DesiredRotation, DeltaSeconds, RotationInterpSpeed));
 }
