5cd1f1b0b2ea9bc232a4a0aba185aac02a334812
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 09:50:11 +0100

    feat: implement football ball possession state

--- Source/FootballLegends3D/Components/FootballBallInteractionComponent.cpp
+++ Source/FootballLegends3D/Components/FootballBallInteractionComponent.cpp
@@ -1,4 +1,5 @@
 #include "Components/FootballBallInteractionComponent.h"
+#include "Ball/FootballBall.h"
 
 UFootballBallInteractionComponent::UFootballBallInteractionComponent()
 {
@@ -8,4 +9,34 @@ UFootballBallInteractionComponent::UFootballBallInteractionComponent()
 bool UFootballBallInteractionComponent::IsBallInControlRange(float DistanceToBall) const
 {
     return DistanceToBall >= 0.0f && DistanceToBall <= ControlRadius;
+}
+
+bool UFootballBallInteractionComponent::CanControlBall(const AFootballBall* Ball) const
+{
+    if (!IsValid(Ball) || !GetOwner())
+    {
+        return false;
+    }
+
+    return IsBallInControlRange(FVector::Dist(GetOwner()->GetActorLocation(), Ball->GetActorLocation()));
+}
+
+void UFootballBallInteractionComponent::SetControlledBall(AFootballBall* NewBall)
+{
+    if (NewBall && !CanControlBall(NewBall))
+    {
+        return;
+    }
+
+    ControlledBall = NewBall;
+}
+
+void UFootballBallInteractionComponent::ReleaseControlledBall()
+{
+    ControlledBall = nullptr;
+}
+
+AFootballBall* UFootballBallInteractionComponent::GetControlledBall() const
+{
+    return ControlledBall.Get();
 }
