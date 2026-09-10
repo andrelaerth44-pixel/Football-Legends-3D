47cfe2dd3bfed6751c984b779c1cff59918a1b10
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 09:13:10 +0100

    feat: add Unreal Engine C++ module rules

---
+++ Source/FootballLegends3D/FootballLegends3D.Build.cs
@@ -0,0 +1,19 @@
+using UnrealBuildTool;
+
+public class FootballLegends3D : ModuleRules
+{
+    public FootballLegends3D(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+    }
+}
