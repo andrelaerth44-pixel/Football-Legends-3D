8ff58cadddda8d428252528908e90864c365669b
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 17:20:44 +0100

    Initialize player team formation data

--- Source/FootballLegends3D/Characters/FootballPlayer.cpp
+++ Source/FootballLegends3D/Characters/FootballPlayer.cpp
@@ -1,6 +1,7 @@
 #include "Characters/FootballPlayer.h"
 #include "Components/PlayerAttributeComponent.h"
 #include "Components/PlayerAppearanceComponent.h"
+#include "Teams/FootballTeamComponent.h"
 #include "Components/FootballBallInteractionComponent.h"
 #include "Characters/FootballPlayerMovementComponent.h"
 #include "Ball/FootballBallGameplayComponent.h"
@@ -14,11 +15,17 @@ AFootballPlayer::AFootballPlayer()
 
     Attributes = CreateDefaultSubobject<UPlayerAttributeComponent>(TEXT("Attributes"));
     Appearance = CreateDefaultSubobject<UPlayerAppearanceComponent>(TEXT("Appearance"));
+    Team = CreateDefaultSubobject<UFootballTeamComponent>(TEXT("Team"));
     BallInteraction = CreateDefaultSubobject<UFootballBallInteractionComponent>(TEXT("BallInteraction"));
     BallGameplay = CreateDefaultSubobject<UFootballBallGameplayComponent>(TEXT("BallGameplay"));
     BallPossession = CreateDefaultSubobject<UFootballBallPossessionComponent>(TEXT("BallPossession"));
     BallTargeting = CreateDefaultSubobject<UFootballBallTargetingComponent>(TEXT("BallTargeting"));
     FootballMovement = CreateDefaultSubobject<UFootballPlayerMovementComponent>(TEXT("FootballMovement"));
+
+    if (Team)
+    {
+        Team->BuildDefaultFormation();
+    }
 
     GetCharacterMovement()->MaxWalkSpeed = 420.0f;
     GetCharacterMovement()->MaxAcceleration = 1800.0f;
