8ced9390c9dafede5a22650aff4bd5a42a63a51b
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 11:49:12 +0100

    Initialize goal replay and presentation components

--- Source/FootballLegends3D/Match/FootballMatchState.cpp
+++ Source/FootballLegends3D/Match/FootballMatchState.cpp
@@ -1,9 +1,13 @@
 #include "Match/FootballMatchState.h"
 #include "Match/FootballMatchScoreComponent.h"
 #include "Match/FootballGoalSequenceComponent.h"
+#include "Match/FootballGoalReplayComponent.h"
+#include "Match/FootballGoalPresentationComponent.h"
 
 AFootballMatchState::AFootballMatchState()
 {
     Score = CreateDefaultSubobject<UFootballMatchScoreComponent>(TEXT("Score"));
     GoalSequence = CreateDefaultSubobject<UFootballGoalSequenceComponent>(TEXT("GoalSequence"));
+    GoalReplay = CreateDefaultSubobject<UFootballGoalReplayComponent>(TEXT("GoalReplay"));
+    GoalPresentation = CreateDefaultSubobject<UFootballGoalPresentationComponent>(TEXT("GoalPresentation"));
 }
