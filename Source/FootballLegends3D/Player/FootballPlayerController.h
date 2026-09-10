21d9822c54ab39786fc8f2736725022f9c7536bf
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 10:13:57 +0100

    feat: add football pass and shot input API

--- Source/FootballLegends3D/Player/FootballPlayerController.h
+++ Source/FootballLegends3D/Player/FootballPlayerController.h
@@ -5,6 +5,9 @@
 #include "FootballPlayerController.generated.h"
 
 class AFootballCameraActor;
+class AFootballBall;
+
+enum class EFootballKickType : uint8;
 
 UCLASS()
 class FOOTBALLLEGENDS3D_API AFootballPlayerController : public APlayerController
@@ -23,10 +26,25 @@ private:
     void MoveRight(float Value);
     void StartSprint();
     void StopSprint();
+
+    void StartPass();
+    void ReleasePass();
+    void StartShot();
+    void ReleaseShot();
+    void Kick(EFootballKickType KickType, float Charge);
+    AFootballBall* FindControllableBall() const;
+    FVector GetKickDirection() const;
     void UpdateMovementSpeed();
 
     UPROPERTY()
     TObjectPtr<AFootballCameraActor> FootballCamera;
 
     bool bSprintHeld = false;
+    bool bPassHeld = false;
+    bool bShotHeld = false;
+    float PassCharge = 0.0f;
+    float ShotCharge = 0.0f;
+
+    UPROPERTY(EditAnywhere, Category="Football|Input", meta=(ClampMin="0.1"))
+    float ChargeRate = 1.6f;
 };
