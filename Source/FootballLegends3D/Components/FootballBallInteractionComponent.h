cfd17d241a03a8331f65f0185c3999ab8c89b0ae
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 09:50:05 +0100

    feat: add player ball possession interface

--- Source/FootballLegends3D/Components/FootballBallInteractionComponent.h
+++ Source/FootballLegends3D/Components/FootballBallInteractionComponent.h
@@ -4,6 +4,8 @@
 #include "Components/ActorComponent.h"
 #include "FootballBallInteractionComponent.generated.h"
 
+class AFootballBall;
+
 /** Entry point for receiving, dribbling, passing and shooting interactions. */
 UCLASS(ClassGroup=(Football), meta=(BlueprintSpawnableComponent))
 class FOOTBALLLEGENDS3D_API UFootballBallInteractionComponent : public UActorComponent
@@ -21,4 +23,20 @@ public:
 
     UFUNCTION(BlueprintPure, Category="Ball")
     bool IsBallInControlRange(float DistanceToBall) const;
+
+    UFUNCTION(BlueprintPure, Category="Ball")
+    bool CanControlBall(const AFootballBall* Ball) const;
+
+    UFUNCTION(BlueprintCallable, Category="Ball")
+    void SetControlledBall(AFootballBall* NewBall);
+
+    UFUNCTION(BlueprintCallable, Category="Ball")
+    void ReleaseControlledBall();
+
+    UFUNCTION(BlueprintPure, Category="Ball")
+    AFootballBall* GetControlledBall() const;
+
+private:
+    UPROPERTY()
+    TObjectPtr<AFootballBall> ControlledBall;
 };
