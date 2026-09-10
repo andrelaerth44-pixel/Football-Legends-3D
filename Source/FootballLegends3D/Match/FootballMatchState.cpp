33d35eb3b372262f27cc13234f2e2da29950853e
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 11:41:57 +0100

    Initialize goal sequence component

--- Source/FootballLegends3D/Match/FootballMatchState.cpp
+++ Source/FootballLegends3D/Match/FootballMatchState.cpp
@@ -1,7 +1,9 @@
 #include "Match/FootballMatchState.h"
 #include "Match/FootballMatchScoreComponent.h"
+#include "Match/FootballGoalSequenceComponent.h"
 
 AFootballMatchState::AFootballMatchState()
 {
     Score = CreateDefaultSubobject<UFootballMatchScoreComponent>(TEXT("Score"));
+    GoalSequence = CreateDefaultSubobject<UFootballGoalSequenceComponent>(TEXT("GoalSequence"));
 }
