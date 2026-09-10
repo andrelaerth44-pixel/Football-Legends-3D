fc67eb338e8b6a93c4c575caf052360da0b63c73
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 11:58:12 +0100

    Wire goal replay director into match state

--- Source/FootballLegends3D/Match/FootballMatchState.cpp
+++ Source/FootballLegends3D/Match/FootballMatchState.cpp
@@ -2,6 +2,7 @@
 #include "Match/FootballMatchScoreComponent.h"
 #include "Match/FootballGoalSequenceComponent.h"
 #include "Match/FootballGoalReplayComponent.h"
+#include "Match/FootballGoalReplayDirectorComponent.h"
 #include "Match/FootballGoalPresentationComponent.h"
 
 AFootballMatchState::AFootballMatchState()
@@ -9,5 +10,6 @@ AFootballMatchState::AFootballMatchState()
     Score = CreateDefaultSubobject<UFootballMatchScoreComponent>(TEXT("Score"));
     GoalSequence = CreateDefaultSubobject<UFootballGoalSequenceComponent>(TEXT("GoalSequence"));
     GoalReplay = CreateDefaultSubobject<UFootballGoalReplayComponent>(TEXT("GoalReplay"));
+    GoalReplayDirector = CreateDefaultSubobject<UFootballGoalReplayDirectorComponent>(TEXT("GoalReplayDirector"));
     GoalPresentation = CreateDefaultSubobject<UFootballGoalPresentationComponent>(TEXT("GoalPresentation"));
 }
