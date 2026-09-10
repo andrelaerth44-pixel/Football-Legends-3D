4def1fe3d1006a66d046d7e95a0d0b90661ff1e1
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 09:52:02 +0100

    feat: initialize player ball gameplay systems

--- Source/FootballLegends3D/Characters/FootballPlayer.cpp
+++ Source/FootballLegends3D/Characters/FootballPlayer.cpp
@@ -3,6 +3,8 @@
 #include "Components/PlayerAppearanceComponent.h"
 #include "Components/FootballBallInteractionComponent.h"
 #include "Characters/FootballPlayerMovementComponent.h"
+#include "Ball/FootballBallGameplayComponent.h"
+#include "Ball/FootballBallPossessionComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 AFootballPlayer::AFootballPlayer()
@@ -12,6 +14,8 @@ AFootballPlayer::AFootballPlayer()
     Attributes = CreateDefaultSubobject<UPlayerAttributeComponent>(TEXT("Attributes"));
     Appearance = CreateDefaultSubobject<UPlayerAppearanceComponent>(TEXT("Appearance"));
     BallInteraction = CreateDefaultSubobject<UFootballBallInteractionComponent>(TEXT("BallInteraction"));
+    BallGameplay = CreateDefaultSubobject<UFootballBallGameplayComponent>(TEXT("BallGameplay"));
+    BallPossession = CreateDefaultSubobject<UFootballBallPossessionComponent>(TEXT("BallPossession"));
     FootballMovement = CreateDefaultSubobject<UFootballPlayerMovementComponent>(TEXT("FootballMovement"));
 
     GetCharacterMovement()->MaxWalkSpeed = 420.0f;
