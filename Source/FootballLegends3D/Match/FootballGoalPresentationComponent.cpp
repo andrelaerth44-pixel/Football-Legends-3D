3d1fdcfe5d237ada58d54edfaa440465ac664d87
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 11:48:52 +0100

    Wire goal presentation to replay

---
+++ Source/FootballLegends3D/Match/FootballGoalPresentationComponent.cpp
@@ -0,0 +1,121 @@
+#include "Match/FootballGoalPresentationComponent.h"
+#include "Match/FootballGoalSequenceComponent.h"
+#include "Match/FootballGoalReplayComponent.h"
+#include "GameFramework/Actor.h"
+
+UFootballGoalPresentationComponent::UFootballGoalPresentationComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UFootballGoalPresentationComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (AActor* Owner = GetOwner())
+    {
+        Sequence = Owner->FindComponentByClass<UFootballGoalSequenceComponent>();
+        Replay = Owner->FindComponentByClass<UFootballGoalReplayComponent>();
+    }
+
+    if (Sequence)
+    {
+        Sequence->OnGoalSequenceStarted.AddDynamic(this, &UFootballGoalPresentationComponent::HandleGoalSequenceStarted);
+        Sequence->OnGoalSequenceFinished.AddDynamic(this, &UFootballGoalPresentationComponent::HandleGoalSequenceFinished);
+    }
+}
+
+void UFootballGoalPresentationComponent::HandleGoalSequenceStarted(
+    EFootballTeamSide ScoringSide,
+    int32 HomeScore,
+    int32 AwayScore,
+    AFootballBall* Ball,
+    float ImpactSpeed,
+    float MatchTimeSeconds)
+{
+    PlayGoalPresentation(ScoringSide, HomeScore, AwayScore, Ball, ImpactSpeed);
+}
+
+void UFootballGoalPresentationComponent::PlayGoalPresentation(
+    EFootballTeamSide ScoringSide,
+    int32 HomeScore,
+    int32 AwayScore,
+    AFootballBall* Ball,
+    float ImpactSpeed)
+{
+    PendingScoringSide = ScoringSide;
+    PendingHomeScore = HomeScore;
+    PendingAwayScore = AwayScore;
+    PendingImpactSpeed = ImpactSpeed;
+    bPresentationActive = true;
+    bPendingReplay = bAutoStartReplay && IsValid(Ball);
+    ReplayDelayRemaining = FMath::Max(0.0f, ReplayDelay);
+
+    if (Replay && Ball)
+    {
+        Replay->SetTrackedBall(Ball);
+    }
+
+    OnPresentationStarted.Broadcast(ScoringSide, HomeScore, AwayScore, Ball, ImpactSpeed);
+    OnScoreboardShown.Broadcast(ScoringSide, HomeScore, AwayScore, Ball, ImpactSpeed);
+
+    if (ReplayDelayRemaining <= KINDA_SMALL_NUMBER)
+    {
+        StartReplayIfReady();
+    }
+}
+
+void UFootballGoalPresentationComponent::StartReplayIfReady()
+{
+    if (!bPendingReplay || !Replay)
+    {
+        return;
+    }
+
+    bPendingReplay = false;
+    Replay->StartReplay();
+}
+
+void UFootballGoalPresentationComponent::StartReplay()
+{
+    if (Replay)
+    {
+        Replay->StartReplay();
+    }
+}
+
+void UFootballGoalPresentationComponent::StopReplay()
+{
+    if (Replay)
+    {
+        Replay->StopReplay();
+    }
+}
+
+void UFootballGoalPresentationComponent::HandleGoalSequenceFinished()
+{
+    bPresentationActive = false;
+    bPendingReplay = false;
+    ReplayDelayRemaining = 0.0f;
+
+    if (Replay && Replay->IsReplaying())
+    {
+        Replay->StopReplay();
+    }
+}
+
+void UFootballGoalPresentationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (!bPresentationActive || !bPendingReplay)
+    {
+        return;
+    }
+
+    ReplayDelayRemaining = FMath::Max(0.0f, ReplayDelayRemaining - FMath::Max(0.0f, DeltaTime));
+    if (ReplayDelayRemaining <= KINDA_SMALL_NUMBER)
+    {
+        StartReplayIfReady();
+    }
+}
