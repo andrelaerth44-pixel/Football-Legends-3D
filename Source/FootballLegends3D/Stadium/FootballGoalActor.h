3364856b863f4cfe939c35ed87483bca95226d41
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 10:40:44 +0100

    feat: add goal detection actor

---
+++ Source/FootballLegends3D/Stadium/FootballGoalActor.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FootballGoalActor.generated.h"
+
+class UBoxComponent;
+class UFootballGoalNetDeformationComponent;
+class AFootballBall;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFootballGoalScoredSignature, AFootballBall*, Ball, float, ImpactSpeed);
+
+/** Goal volume that detects a complete ball entry and emits a goal event. */
+UCLASS()
+class FOOTBALLLEGENDS3D_API AFootballGoalActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AFootballGoalActor();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Goal")
+    TObjectPtr<UBoxComponent> GoalVolume;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Goal")
+    TObjectPtr<UFootballGoalNetDeformationComponent> NetReaction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Goal", meta=(ClampMin="0.05"))
+    float GoalCooldown = 1.0f;
+
+    UPROPERTY(BlueprintAssignable, Category="Goal")
+    FFootballGoalScoredSignature OnGoalScored;
+
+    UFUNCTION(BlueprintCallable, Category="Goal")
+    void ResetGoal();
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UFUNCTION()
+    void OnGoalVolumeBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    float LastGoalTime = -1000.0f;
+};
