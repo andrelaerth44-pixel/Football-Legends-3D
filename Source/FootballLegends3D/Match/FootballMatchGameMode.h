fa5f0b2e4a70d130cfeef91ecfb683aafac9631e
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 11:23:09 +0100

    Add football match game mode

---
+++ Source/FootballLegends3D/Match/FootballMatchGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FootballMatchGameMode.generated.h"
+
+UCLASS()
+class FOOTBALLLEGENDS3D_API AFootballMatchGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AFootballMatchGameMode();
+};
