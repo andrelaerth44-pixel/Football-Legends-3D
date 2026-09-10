1cca9f2b65da87f640ad74b713801514ffcc2dce
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-10 09:14:25 +0100

    feat: configure football character movement defaults

--- Source/FootballLegends3D/Characters/FootballPlayer.cpp
+++ Source/FootballLegends3D/Characters/FootballPlayer.cpp
@@ -2,6 +2,8 @@
 #include "Components/PlayerAttributeComponent.h"
 #include "Components/PlayerAppearanceComponent.h"
 #include "Components/FootballBallInteractionComponent.h"
+#include "Characters/FootballPlayerMovementComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AFootballPlayer::AFootballPlayer()
 {
@@ -10,4 +12,11 @@ AFootballPlayer::AFootballPlayer()
     Attributes = CreateDefaultSubobject<UPlayerAttributeComponent>(TEXT("Attributes"));
     Appearance = CreateDefaultSubobject<UPlayerAppearanceComponent>(TEXT("Appearance"));
     BallInteraction = CreateDefaultSubobject<UFootballBallInteractionComponent>(TEXT("BallInteraction"));
+    FootballMovement = CreateDefaultSubobject<UFootballPlayerMovementComponent>(TEXT("FootballMovement"));
+
+    GetCharacterMovement()->MaxWalkSpeed = 420.0f;
+    GetCharacterMovement()->MaxAcceleration = 1800.0f;
+    GetCharacterMovement()->BrakingDecelerationWalking = 1600.0f;
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    bUseControllerRotationYaw = false;
 }
